a299a74fb62b06f9b3266e361e605aa18c64669a
rogelio1502 <rogelio15021515@gmail.com>
2023-04-24 11:00:35 -0600

    updated Service Provider

--- src/EF/Providers/EfServiceProvider.php
+++ src/EF/Providers/EfServiceProvider.php
@@ -13,6 +13,15 @@ class EfServiceProvider extends ServiceProvider
         $ds->registerResource('scripts','/ef/js/ef-orchid-package.js');
         $ds->registerResource('stylesheets','/ef/css/ef-orchid-package.css');
         $this->loadViewsFrom(__DIR__ . '/../../../resources/views', 'ef');
+        $this->publishes([
+            __DIR__.'/../../../resources/views/vendor' => resource_path('views/vendor'),
+        ]);
+        $this->publishes([
+            __DIR__.'/../../../resources/views/brand' => resource_path('views/brand'),
+        ]);
+        $this->publishes([
+            __DIR__.'/../../../public/ef' => public_path('ef'),
+        ], 'public');
     }
     
 }
